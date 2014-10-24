5f6a6b78a5841c52a8cf3289c6828eb382a40ae2
Elaine <elainezuniga@gmail.com>
2014-10-24 17:38:08 -0500

    js script added

--- sample.js
+++ sample.js
@@ -1,6 +1,5 @@
 $(function() {
 
-
     // TODO #1 Set an event listener to listen for clicks on each menu section
     // heading anchor
 
@@ -21,20 +20,19 @@ $(function() {
       // Prevent the default action of the event
       event.preventDefault();
 
-      // Assign the id of the clicked element (this) to a variable named id
-      var  id = $( this ).attr( 'id' );
+      // Assign the id of the clicked element to a variable named id
+      var id = $( this ).attr( 'id' );
 
       // Remove the class 'is-active' from all menu item headings
-      $( 'menu-section-item' ).removeClass( 'is-active' );
+      $( '.menu-section-item' ).removeClass( 'is-active' );
 
-      // Add 'is-active' to this specific action that was clicked (this). is-active
+      // Add 'is-active' to this specific action that was clicked. is-active
       // provides the visual cue for what's active via CSS
       $( this ).addClass( 'is-active' );
 
       // Once you're started with TODO #2, call the getMenu function here,
       // passing id as the argument
       getMenu( id );
-
     });
 
 
@@ -53,14 +51,13 @@ $(function() {
     // menu for into this function
 
     function getMenu( course ) {
-     // Use `$.getJSON` to get the menu for whatever menu heading was clicked
-     $.getJSON( './json/menu-' + course + '.json', function( json ) {
-       populateMenu( json );
-       // Once you're started with TODO #3, call the populateMenu function here
-       // and pass json as the argument
-     });
+      // Use `$.getJSON` to get the menu for whatever menu heading was clicked
+      $.getJSON( './json/menu-' + course + '.json', function( json ) {
+        populateMenu( json );
+        // Once you're started with TODO #3, call the populateMenu function here
+        // and pass json as the argument
+      });
     }
-
 
 
 
@@ -69,23 +66,12 @@ $(function() {
     function populateMenu( json ) {
       html = '';
 
-      // start a for loop that iterates through json.length
-      // add json.length into this for loop code
       for( var i = 0; i < json.length; i++ ){
-        // wrap each section in a menu-group div
         html += '<div class="menu-group columns small-12 medium-4">';
-        // append inside the menu-group div a h4 with the json section name in it
-        // all the content in this for loop will follow this same model:
-        // put the correct content in between the blank + + signs.
         html += '<h4>' + json[i].section + '</h4>';
 
-        // inside each menu-group section, start a for loop that
-        // prints out each menu-item div
         for( var j = 0; j < json[i].content.length; j++ ) {
-          // for each menu item in json[i].content, create a menu-item div
           html += '<div class="menu-item">';
-          // inside each menu-item div, create a div for dish, ingredients, and price
-          // add json[i]content[j].THING where THING is dish, ingredient, price.
           html += '<div class="menu-item-dish">' + json[i].content[j].dish + '</div>';
           html += '<p class="menu-item-ingredients">' + json[i].content[j].ingredients + '</p>';
           html += '<div class="menu-item-price">' + json[i].content[j].price + '</div>';
@@ -103,6 +89,5 @@ $(function() {
 
     // TODO #4 Call getMenu with a menu of your choice and set that menu's
     // header to active so that a menu is loaded with the page by default
-    getMenu('breakfast');
-    $( '.menu-section#breakfast' ).addClass( 'is-active' );
+
 });
